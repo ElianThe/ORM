55e54d68e9fa15fc9edf0ca14f950711b9872376
lucarafe <luc.bonin0903@gmail.com>
2023-12-05 14:26:11 +0100

    doctrine classe produit.php

---
+++ Doctrine/src/modele/produit.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace catadoct\catalog;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToOne;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity]
+#[Table(name: "produit")]
+class Produit
+{
+    #[Id]
+    #[Column(type: Types::INTEGER)]
+    #[GeneratedValue(strategy: "AUTO")]
+    private int $id;
+
+    #[Column(type: Types::INTEGER)]
+    private int $numero;
+
+    #[Column(type: Types::STRING)]
+    private string $libelle;
+
+    #[Column(type: Types::STRING)]
+    private string $description;
+
+    #[Column(type: Types::STRING)]
+    private string $image;
+
+    #[OneToOne(targetEntity: Categorie::class)]
+    private string $categorie_id;
+}
